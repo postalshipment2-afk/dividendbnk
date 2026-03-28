541578662ba0e0bce96536958d4d5051dc9dc5a2
Uchenna ogbu <ogbu41@gmail.com>
2026-03-28 04:26:30 +0300

    fixing vercel routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-// import babel from '@rolldown/plugin-babel'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     babel({ presets: [reactCompilerPreset()] })
-//   ],
-// })
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         manualChunks(id) {
-//           if (id.includes("node_modules")) {
-//             return "vendor";
-//           }
-//         },
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
